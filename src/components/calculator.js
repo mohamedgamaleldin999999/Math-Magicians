767f3c259663b88d4df874b7d8b1da3a3aa326f0
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-11 12:31:26 +0300

    Add mark up and styling

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,6 +5,37 @@ function Calculator() {
   return (
     <div className="calculator">
       <div className='result'>0</div>
+      <div className='btns'>
+        <div className='row'>
+          <button className='btn'>AC</button>
+          <button className='btn'>+/-</button>
+          <button className='btn'>%</button>
+          <button className='btn'>+</button>
+        </div>
+        <div className='row'>
+          <button className='btn'>7</button>
+          <button className='btn'>8</button>
+          <button className='btn'>9</button>
+          <button className='btn'>*</button>
+        </div>
+        <div className='row'>
+          <button className='btn'>4</button>
+          <button className='btn'>5</button>
+          <button className='btn'>6</button>
+          <button className='btn'>-</button>
+        </div>
+        <div className='row'>
+          <button className='btn'>1</button>
+          <button className='btn'>2</button>
+          <button className='btn'>3</button>
+          <button className='btn'>+</button>
+        </div>
+        <div className='row'>
+          <button className='btn'>0</button>
+          <button className='btn'>.</button>
+          <button className='btn'>=</button>
+        </div>
+      </div>
     </div>
   );
 }
