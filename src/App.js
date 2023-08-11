cd92dddff340f34a5ebd48ad89f34081a18cd8da
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-11 03:47:42 +0300

    Added first div

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './components/calculator';
+import './App.css'; // You can create this CSS file for global styles
+
+function App() {
+  return (
+    <div className="app">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './calculator.css';
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className='result'>0</div>
+    </div>
+  );
+}
+
+export default Calculator;
