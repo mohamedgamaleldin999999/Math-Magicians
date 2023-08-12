a4e9a451fcc262e9f888d7d092b2ad45012aa94a
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-12 16:51:52 +0300

    Add component and export

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,2 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
+const QuoteComponent = () => {
+  const [quote, setQuote] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const category = 'happiness';
+    axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+      headers: { 'X-Api-Key': 'YOUR_API_KEY' },
+    })
+    .then(response => {
+      setQuote(response.data[0].quote);
+      setLoading(false);
+    })
+    .catch(error => {
+      setError(error);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h2>Quote of the Day</h2>
+      <p>{quote}</p>
+    </div>
+  );
+};
+
+export default QuoteComponent;
