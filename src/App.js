ca5857f1e186f75de6f82962b48f73be8e0e8c5f
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-16 01:40:37 +0300

    Add router functionality

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Calculator from './components/calculator';
 import './App.css';
 import QuoteComponent from './components/quote';
 import { Header } from './components/header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home';
 
 function App() {
   return (
-    <div className="app">
+    <Router>
       <Header />
-      <Calculator />
-      <QuoteComponent />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<QuoteComponent />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import './header.css';
+import { Link } from 'react-router-dom';
 
 export function Header() {
   return (
@@ -6,9 +7,11 @@ export function Header() {
       <h1>Math Magitians</h1>
       <nav className="nav">
         <ul>
-          <li>Home</li>
-          <li>Calculator</li>
-          <li>Quote</li>
+          <li><Link to="/">Home</Link></li>
+          <li>|</li>
+          <li><Link to="/calculator">Calculator</Link></li>
+          <li>|</li>
+          <li><Link to="/quotes">Quotes</Link></li>
         </ul>
       </nav>
     </div>
