d2d5517d791f82e0d5c1fb55ae0bc0080f4466fc
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-15 23:52:17 +0300

    Create header mark up

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 import QuoteComponent from './components/quote';
+import { Header } from './components/header';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Calculator />
       <QuoteComponent />
     </div>
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+export function Header() {
+  return (
+    <div>
+      <h1>Math Magitians</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Calculator</li>
+          <li>Quote</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
